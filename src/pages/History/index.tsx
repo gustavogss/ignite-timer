813f6dc6e564c05a27d6f355f088e68dfda67cee
gustavogss <gustavogss.dev@gmail.com>
2024-03-12 17:38:08 -0300

    feat: history page creator

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,41 @@
-import { Container } from './styles'
+import { Container, HistoryList, Status } from './styles'
 
 export function History() {
   return (
     <Container>
-      <p>History</p>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Ínicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>05 minutos</td>
+              <td>começou hoje</td>
+              <td><Status statusColor='yellow'>Andamento</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>02 minutos</td>
+              <td>há 1 semana</td>
+              <td><Status statusColor='red'>Interrompido</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 1 mês</td>
+              <td><Status statusColor='green'>Concluído</Status></td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
     </Container>
   )
 }
